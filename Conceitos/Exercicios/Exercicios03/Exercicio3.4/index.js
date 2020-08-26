d6852d330617105ec5740b89cda54f033cf6d15f
Ricardo <ricardo.baldrez@gmail.com>
2020-08-26 15:50:05 -0300

    Conclusão do exercício 3.4

--- Conceitos/Exercicios/Exercicios03/Exercicio3.4/index.js
+++ Conceitos/Exercicios/Exercicios03/Exercicio3.4/index.js
@@ -1,37 +1,21 @@
 "use strict";
 
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
-
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-var usuarios = [{
-  nome: 'Diego',
-  idade: 23,
-  empresa: 'Rocketseat'
-}, {
-  nome: 'Gabriel',
-  idade: 15,
-  empresa: 'Rocketseat'
-}, {
-  nome: 'Lucas',
-  idade: 30,
-  empresa: 'Facebook'
-}]; // const users = [];
-// const user = usuarios.map(function (user) {
-//     user.idade *= 2;
-//     users.push(user);
-// });
-// console.log(users);
-// const userAgeSm = users.filter(user => user.idade < 50);
-// console.log(userAgeSm);
-
-var calculo = usuarios.map(function (user) {
-  return _objectSpread(_objectSpread({}, user), {}, {
-    idade: user.idade * 2
+// const promise = function () {
+//     return new Promise(function (resolve, reject) {
+//         return resolve();
+//     });
+// };
+var promise = function promise() {
+  return new Promise(function (resolve, reject) {
+    return resolve();
   });
-}).filter(function (user) {
-  return user.idade <= 50;
+};
+
+console.log(promise);
+promise().then(function (response) {
+  console.log(response);
+  console.log('Tudo certo, continue ...');
+})["catch"](function (error) {
+  console.log(error);
+  console.log("Tudo errado, pare e volte ...");
 });
-console.log(calculo);
--- Conceitos/Exercicios/Exercicios03/Exercicio3.4/main.js
+++ Conceitos/Exercicios/Exercicios03/Exercicio3.4/main.js
@@ -1,21 +1,19 @@
-const usuarios = [
-    { nome: 'Diego', idade: 23, empresa: 'Rocketseat' },
-    { nome: 'Gabriel', idade: 15, empresa: 'Rocketseat' },
-    { nome: 'Lucas', idade: 30, empresa: 'Facebook' },
-];
+// const promise = function () {
+//     return new Promise(function (resolve, reject) {
+//         return resolve();
+//     });
+// };
 
-// const users = [];
+const promise = () => new Promise((resolve, reject) => resolve());
 
-// const user = usuarios.map(function (user) {
-    
-//     user.idade *= 2;
-//     users.push(user);
-// });
+console.log(promise);
 
-// console.log(users);
-
-// const userAgeSm = users.filter(user => user.idade < 50);
-// console.log(userAgeSm);
-
-const calculo = usuarios.map(user => ({...user, idade : user.idade * 2})).filter(user => user.idade <= 50)
-console.log(calculo);
+promise()
+    .then(function (response) {
+        console.log(response);
+        console.log('Tudo certo, continue ...');
+    })
+    .catch(function (error) {
+        console.log(error);
+        console.log("Tudo errado, pare e volte ...");
+    })
