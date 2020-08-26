c302b6f6692474d4b43a7925c17c442f884616c7
Ricardo <ricardo.baldrez@gmail.com>
2020-08-26 17:54:00 -0300

    Conclusão do exercício 5.2

--- Conceitos/Exercicios/Exercicios05/Exercicio5.2/index.js
+++ Conceitos/Exercicios/Exercicios05/Exercicio5.2/index.js
@@ -1,18 +1,33 @@
 "use strict";
 
-// function mostraInfo(usuario) {
-//     return `${usuario.nome} tem ${usuario.idade} anos.`;
-// }
-// mostraInfo({ nome: "Diego", idade: 23 });
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+// A partir do objeto e utilizando o operador spread:
 var usuario = {
   nome: 'Diego',
-  idade: 23
-};
+  idade: 23,
+  endereco: {
+    cidade: 'Rio do Sul',
+    uf: 'SC',
+    pais: 'Brasil'
+  }
+}; // Crie uma variável usuario2 que contenha todos os dados do usuário porém com nome Gabriel.
+// Crie uma variável usuario3 que contenha todos os dados do usuário porém com cidade Lontras.
 
-var mostraInfo = function mostraInfo(_ref) {
-  var nome = _ref.nome,
-      idade = _ref.idade;
-  return "".concat(nome, " tem ").concat(idade, " anos.");
-};
+var usuario2 = _objectSpread(_objectSpread({}, usuario), {}, {
+  nome: 'Gabriel'
+});
 
-console.log(mostraInfo(usuario));
+var usuario3 = _objectSpread(_objectSpread({}, usuario), {}, {
+  endereco: _objectSpread(_objectSpread({}, usuario.endereco), {}, {
+    cidade: 'Lontras'
+  })
+});
+
+console.log(usuario);
+console.log(usuario2);
+console.log(usuario3);
--- Conceitos/Exercicios/Exercicios05/Exercicio5.2/main.js
+++ Conceitos/Exercicios/Exercicios05/Exercicio5.2/main.js
@@ -1,14 +1,19 @@
-// function mostraInfo(usuario) {
-//     return `${usuario.nome} tem ${usuario.idade} anos.`;
-// }
-
-// mostraInfo({ nome: "Diego", idade: 23 });
-
+// A partir do objeto e utilizando o operador spread:
 const usuario = {
     nome: 'Diego',
-    idade: 23
-}
+    idade: 23,
+    endereco: {
+        cidade: 'Rio do Sul',
+        uf: 'SC',
+        pais: 'Brasil',
+    }
+};
+// Crie uma variável usuario2 que contenha todos os dados do usuário porém com nome Gabriel.
+// Crie uma variável usuario3 que contenha todos os dados do usuário porém com cidade Lontras.
 
-const mostraInfo = ({nome, idade}) => `${nome} tem ${idade} anos.`;
+const usuario2 = { ...usuario, nome: 'Gabriel' };
+const usuario3 = { ...usuario,  endereco: { ...usuario.endereco, cidade: 'Lontras' }}
 
-console.log(mostraInfo(usuario));
+console.log(usuario);
+console.log(usuario2);
+console.log(usuario3);
