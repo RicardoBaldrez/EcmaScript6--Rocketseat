89d64b668e7b26a97f20b7525b077a04f15b7a16
Ricardo <ricardo.baldrez@gmail.com>
2020-08-26 18:04:02 -0300

    Inclusão dos exercícios 06 + conclusão do exercício - Template Literals

---
+++ Conceitos/Exercicios/Exercicios06/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var usuario = "Diego";
+var idade = 23;
+console.log("O usuário " + usuario + " possui " + idade + " anos"); // Transformando em Template Literals
+
+console.log("O usu\xE1rio ".concat(usuario, " possui ").concat(idade, " anos"));
