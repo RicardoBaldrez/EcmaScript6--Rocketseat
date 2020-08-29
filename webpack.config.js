50e03a57f695fd7715c26cff13a9db7007caf5d8
Ricardo <ricardo.baldrez@gmail.com>
2020-08-29 00:20:38 -0300

    package.json + webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    // entry -> Dizendo qual é o arquivo principal
+    entry: './src/main.js',
+    // Dizendo para qual arquivo a conversão do ES6+ vai  
+    output: {
+        // Pegando a raiz do nosso projeto
+        path: __dirname + '/public/',
+        filename: 'bundle.js',
+    },
+    devServer: {
+        // Caminho que utilizará para levantar o servidor
+        contentBase: __dirname + '/public/',
+    },
+    module: {
+        // Propriedade obrigatória, que mostra como o webpack deve se comportar quando o usuário estiver tentando importar outros arquivos js
+        rules: [
+            {
+                // Toda vez então que tiver um novo arquivo js importado, o babel será executado automaticamente
+                // Expressão Regular verificando se o arquivo terminar com .js
+                test: /\.js$/,
+                // Retirando a pasta node_modules da conversão
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ],
+    },
+};
